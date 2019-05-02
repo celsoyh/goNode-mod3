1c27689fbd228e7298e7dcc1a1066d0bdd6fa169
celsoyh <celsoyh@gmail.com>
2019-05-01 22:58:13 -0300

    Init Mongodb config

--- src/server.js
+++ src/server.js
@@ -1,22 +1,32 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const databaseCfg = require('./config/database')
 
 class App {
-	constructor() {
-		this.express = express()
+  constructor() {
+    this.express = express()
 
-		this.middlewares()
-		this.routes()
-	}
+    this.database()
+    this.middlewares()
+    this.routes()
+  }
 
-	middlewares() {
-		this.express.use(express.json())
-	}
+  database() {
+    mongoose.connect(databaseCfg.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+    })
+  }
 
-	routes() {
-		this.express.use('/', (req, res) => {
-			return res.send('hello')
-		})
-	}
+  middlewares() {
+    this.express.use(express.json())
+  }
+
+  routes() {
+    this.express.use('/', (req, res) => {
+      return res.send('hello')
+    })
+  }
 }
 
 module.exports = new App().express
